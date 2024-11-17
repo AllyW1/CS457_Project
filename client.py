1ff8984743c5d06ea5ed663afc87f9ca1c270804
AllyW1 <142823428+AllyW1@users.noreply.github.com>
2024-11-17 16:04:19 -0700

    SP4

--- client.py
+++ client.py
@@ -11,7 +11,7 @@ class TicTacToeClient:
 
     def connect_to_server(self):
         self.socket.connect((self.server_ip, self.server_port))
-        print(f'Connected to Tic-Tac-Toe server at {self.server_ip}:{self.server_port}')
+        print(f"Connected to Tic-Tac-Toe server at {self.server_ip}:{self.server_port}")
 
     def receive_messages(self):
         try:
@@ -31,10 +31,11 @@ class TicTacToeClient:
                         print("Failed to decode JSON:", message_str)
                         continue
 
-                    print(message.get('message'))
-
                     if message['type'] == 'username_request':
                         self.send_username()
+                        continue
+
+                    print(message.get('message'))
 
                     if 'board' in message:
                         print(message['board'])
@@ -42,12 +43,12 @@ class TicTacToeClient:
                     if "Your move." in message.get('message', ''):
                         self.send_move()
 
-                    if "Game Over!" in message.get('message', ''):
-                        break
+                    if message.get('type') == 'prompt_restart':
+                        self.handle_game_over()
+                        continue
 
-                    if message['type'] == 'disconnect_notice':
-                        print("The other player has disconnected.")
-                        self.handle_disconnect_option()
+                    if message.get('type') == 'end':
+                        print("The game has ended. Disconnecting...")
                         return
         except (ConnectionResetError, BrokenPipeError):
             print("Connection to the server has been lost.")
@@ -61,37 +62,50 @@ class TicTacToeClient:
         self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
 
     def send_move(self):
-        move = input("Enter your move (1-9, q to quit): ")
-        if move.lower() == 'q':
-            message = {'type': 'quit'}
-            self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
-        else:
+        while True:
+            move = input("Enter your move (1-9, q to quit): ").strip()
+            if move.lower() == 'q':
+                message = {'type': 'quit'}
+                self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
+                print("You chose to quit. Disconnecting...")
+                self.close_connection()
+                return
             try:
                 move = int(move)
-                message = {'type': 'move', 'position': move}
-                self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
+                if 1 <= move <= 9:
+                    message = {'type': 'move', 'position': move}
+                    self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
+                    break
+                else:
+                    print("Invalid input. Enter a number between 1 and 9.")
             except ValueError:
                 print("Invalid input. Please enter a valid move.")
-                self.send_move()
 
-    def handle_disconnect_option(self):
+    def handle_game_over(self):
+        print("Game over!")
         while True:
-            choice = input("The other player has left the game. Do you want to quit? (Type 'q' to quit): ")
-            if choice.lower() == 'q':
-                print("You have chosen to quit the game.")
-                self.close_connection()
-                break
+            decision = input("Would you like to play again? (y/n): ").strip().lower()
+            if decision in ('y', 'n'):
+                message = {'type': 'restart_decision', 'decision': decision}
+                self.socket.sendall((json.dumps(message) + '\n').encode('utf-8'))
+                if decision == 'n':
+                    print("You chose to quit. Disconnecting from the game...")
+                    self.close_connection()
+                    return
+                else:
+                    print("Waiting for the other player to decide...")
+                    return
             else:
-                print("Invalid input. Type 'q' to quit.")
+                print("Invalid input. Please type 'y' or 'n'.")
 
     def close_connection(self):
         self.socket.close()
         print('Disconnected from server.')
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Client for the Tic-Tac-Toe game.')
-    parser.add_argument('host', type=str, help='Host IP address to connect to')
-    parser.add_argument('port', type=int, help='Port number to connect on')
+    parser = argparse.ArgumentParser(description="Client for the Tic-Tac-Toe game.")
+    parser.add_argument('-i', '--host', type=str, required=True, help="Host IP address or DNS to connect to.")
+    parser.add_argument('-p', '--port', type=int, required=True, help="Port number to connect on.")
     args = parser.parse_args()
 
     client = TicTacToeClient(args.host, args.port)
--- server.py
+++ server.py
@@ -3,20 +3,22 @@ import socket
 import json
 import argparse
 import logging
+import time
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 class TicTacToeServer:
-    def __init__(self, host, port):
+    def __init__(self, port, timeout=60):
         self.selector = selectors.DefaultSelector()
-        self.host = host
+        self.host = '0.0.0.0'  # Listen on all network interfaces
         self.port = port
         self.clients = {}
         self.board = [' '] * 9
         self.player_symbols = ['X', 'O']
         self.turn_index = 0
         self.game_over = False
-        self.running = True
+        self.last_activity = time.time()
+        self.timeout = timeout
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,17 +27,31 @@ class TicTacToeServer:
         self.server_socket.listen()
         self.server_socket.setblocking(False)
         self.selector.register(self.server_socket, selectors.EVENT_READ)
-        logging.info(f'Server started, listening on {self.host}:{self.port}')
+        logging.info(f"Server started on {self.host}:{self.port}")
 
     def accept_client(self):
         client_socket, addr = self.server_socket.accept()
-        logging.info(f'Accepted connection from {addr}')
+        logging.info(f"Accepted connection from {addr}")
         client_socket.setblocking(False)
         self.selector.register(client_socket, selectors.EVENT_READ, data=addr)
-        self.send_message(client_socket, {'type': 'username_request', 'message': "Please enter a username:"})
+
+        if len(self.clients) == 0:
+            self.reset_game()
+
+        if client_socket not in self.clients:
+            self.clients[client_socket] = {'username': None, 'restart_decision': None}
+            self.send_message(client_socket, {'type': 'username_request', 'message': "Please enter a username:"})
+        self.update_last_activity()
+
+    def send_message(self, client_socket, message):
+        try:
+            message_str = json.dumps(message) + '\n'
+            client_socket.sendall(message_str.encode('utf-8'))
+        except (ConnectionResetError, BrokenPipeError):
+            self.disconnect_client(client_socket)
 
     def broadcast(self, message, include_board=True, include_turn_prompt=False):
-        for client, client_data in self.clients.items():
+        for client, data in self.clients.items():
             final_message = message.copy()
             if include_board:
                 final_message['board'] = self.get_board_string()
@@ -43,26 +59,18 @@ class TicTacToeServer:
                 final_message['message'] += " Your move."
             self.send_message(client, final_message)
 
-    def send_message(self, client_socket, message):
-        try:
-            message_str = json.dumps(message) + '\n'
-            client_socket.sendall(message_str.encode('utf-8'))
-            logging.info(f"Sent message to client: {message}")
-        except (ConnectionResetError, BrokenPipeError):
-            self.disconnect_client(client_socket)
-
     def get_current_player(self):
-        for client, data in self.clients.items():
-            if data['symbol'] == self.player_symbols[self.turn_index]:
-                return client
+        return next((client for client, data in self.clients.items() if 'symbol' in data and data['symbol'] == self.player_symbols[self.turn_index]), None)
 
     def get_board_string(self):
         key_grid = [" 1 | 2 | 3 ", "---+---+---", " 4 | 5 | 6 ", "---+---+---", " 7 | 8 | 9 "]
-        current_board = [" {} | {} | {} ".format(self.board[0], self.board[1], self.board[2]),
-                         "---+---+---",
-                         " {} | {} | {} ".format(self.board[3], self.board[4], self.board[5]),
-                         "---+---+---",
-                         " {} | {} | {} ".format(self.board[6], self.board[7], self.board[8])]
+        current_board = [
+            " {} | {} | {} ".format(self.board[0], self.board[1], self.board[2]),
+            "---+---+---",
+            " {} | {} | {} ".format(self.board[3], self.board[4], self.board[5]),
+            "---+---+---",
+            " {} | {} | {} ".format(self.board[6], self.board[7], self.board[8])
+        ]
         combined_str = '\n'.join([f"{key_grid[i]}    {current_board[i]}" for i in range(len(key_grid))])
         return f"Position Key:  Current Board:\n{combined_str}"
 
@@ -71,116 +79,130 @@ class TicTacToeServer:
         for a, b, c in win_conditions:
             if self.board[a] == self.board[b] == self.board[c] and self.board[a] != ' ':
                 return self.board[a]
-        if ' ' not in self.board:
-            return 'draw'
-        return None
+        return 'draw' if ' ' not in self.board else None
 
     def handle_game_logic(self, key, mask):
         sock = key.fileobj
-        data = sock.recv(1024).decode('utf-8')
-        if not data:
+        try:
+            data = sock.recv(1024).decode('utf-8')
+            if not data:
+                self.disconnect_client(sock)
+                return
+        except ConnectionResetError:
             self.disconnect_client(sock)
             return
-        
+
         try:
             message = json.loads(data)
-            logging.info(f"Received message from client: {message}")
         except json.JSONDecodeError:
             self.send_message(sock, {'type': 'error', 'message': "Invalid message format."})
             return
 
-        if self.game_over:
-            self.send_message(sock, {'type': 'error', 'message': "The game is over. Please restart."})
-            return
-
-        if 'type' in message and message['type'] == 'username_response':
+        if message.get('type') == 'username_response':
             self.assign_username(sock, message.get('username', ''))
 
-        elif message['type'] == 'move':
+        elif message.get('type') == 'move':
             if sock != self.get_current_player():
                 self.send_message(sock, {'type': 'error', 'message': "Wait for your turn."})
             else:
                 pos = message.get('position')
                 if 1 <= pos <= 9 and self.board[pos - 1] == ' ':
                     self.board[pos - 1] = self.player_symbols[self.turn_index]
-                    self.broadcast({'type': 'move', 'message': f"{self.clients[sock]['username']} placed {self.player_symbols[self.turn_index]}."}, include_board=True)
-                    
                     winner = self.check_winner()
-                    if winner == 'draw':
-                        self.broadcast({'type': 'end', 'message': "Game Over! It's a draw!"}, include_board=True)
+                    if winner:
                         self.game_over = True
-                        self.end_game()
-                    elif winner:
-                        self.broadcast({'type': 'end', 'message': f"Game Over! {self.clients[sock]['username']} ({winner}) wins!"}, include_board=True)
-                        self.game_over = True
-                        self.end_game()
+                        self.broadcast({'type': 'end', 'message': f"Game over! {winner} wins!"}, include_board=True)
+                        self.prompt_restart()
                     else:
                         self.turn_index = 1 - self.turn_index
-                        self.broadcast({'type': 'turn', 'message': f"{self.clients[self.get_current_player()]['username']}'s turn."}, include_board=True, include_turn_prompt=True)
+                        self.broadcast({'type': 'turn', 'message': "Next player's turn."}, include_board=True, include_turn_prompt=True)
                 else:
-                    self.send_message(sock, {'type': 'error', 'message': "Invalid move or position taken, try again."})
-                    self.broadcast({'type': 'turn', 'message': f"Invalid move or position taken. {self.clients[self.get_current_player()]['username']}'s turn."}, include_board=True)
+                    self.send_message(sock, {'type': 'error', 'message': "Invalid move. Try again."})
 
-        elif message['type'] == 'quit':
-            self.broadcast({'type': 'end', 'message': f"{self.clients[sock]['username']} has quit the game."}, include_board=True)
-            self.disconnect_client(sock)
+        elif message.get('type') == 'restart_decision':
+            decision = message.get('decision')
+            self.clients[sock]['restart_decision'] = decision
+            if all('restart_decision' in c for c in self.clients.values()):
+                if all(c['restart_decision'] == 'y' for c in self.clients.values()):
+                    self.reset_game()
+                    self.broadcast({'type': 'start', 'message': "Both players agreed to restart the game!"}, include_board=True, include_turn_prompt=True)
+                else:
+                    self.end_game()
+
+        elif message.get('type') == 'quit':
+            self.broadcast({'type': 'end', 'message': "The game has ended due to a player quitting."})
             self.end_game()
 
+    def prompt_restart(self):
+        self.broadcast({'type': 'prompt_restart', 'message': "Would you like to play again? (y/n)"}, include_board=False)
+
+    def reset_game(self):
+        self.board = [' '] * 9
+        self.turn_index = 0
+        self.game_over = False
+        for client in self.clients.values():
+            client.pop('restart_decision', None)
+
     def assign_username(self, sock, username):
-        if not username:
-            username = f"Player {len(self.clients) + 1}"
-        self.clients[sock] = {
-            'username': username,
-            'symbol': self.player_symbols[len(self.clients) % 2],
-        }
+        self.clients[sock]['username'] = username
+        self.clients[sock]['symbol'] = self.player_symbols[len(self.clients) - 1]
         self.send_message(sock, {'type': 'response', 'message': f"Welcome {username}. You are {self.clients[sock]['symbol']}."})
         if len(self.clients) == 2:
-            self.broadcast({
-                'type': 'start',
-                'message': f"Both players connected. {self.clients[self.get_current_player()]['username']}'s turn.",
-                'board': self.get_board_string()
-            }, include_turn_prompt=True)
+            self.broadcast({'type': 'start', 'message': "Game starts now!"}, include_turn_prompt=True)
 
-    def disconnect_client(self, client_socket):
-        if client_socket in self.clients:
-            username = self.clients[client_socket]['username']
-            logging.info(f"Disconnecting {username}")
-            self.selector.unregister(client_socket)
-            client_socket.close()
-            del self.clients[client_socket]
-            if len(self.clients) == 1:
-                remaining_client = next(iter(self.clients))
-                self.send_message(remaining_client, {'type': 'disconnect_notice', 'message': f"{username} has left the game."})
+    def disconnect_client(self, sock):
+        if sock in self.clients:
+            del self.clients[sock]
+        try:
+            self.selector.unregister(sock)
+        except KeyError:
+            logging.info("Socket already unregistered.")
+        try:
+            sock.close()
+        except OSError:
+            logging.info("Socket already closed.")
+        logging.info(f"Client disconnected.")
+
+    def update_last_activity(self):
+        self.last_activity = time.time()
+
+    def check_timeout(self):
+        if time.time() - self.last_activity > self.timeout:
+            logging.info("Server inactive for too long. Shutting down.")
+            self.shutdown()
+
+    def shutdown(self):
+        logging.info("Shutting down server.")
+        for client in list(self.clients.keys()):
+            self.disconnect_client(client)
+        self.selector.unregister(self.server_socket)
+        self.server_socket.close()
+        exit(0)
 
     def end_game(self):
+        logging.info("Ending the game and shutting down the server.")
         for client in list(self.clients.keys()):
-            self.send_message(client, {'type': 'end', 'message': "Game has ended. Disconnecting..."})
+            self.send_message(client, {'type': 'end', 'message': "The game has ended."})
             self.disconnect_client(client)
-        self.running = False
+        self.selector.unregister(self.server_socket)
+        self.server_socket.close()
+        exit(0)
 
     def run(self):
-        try:
-            while self.running:
-                events = self.selector.select(timeout=None)
-                for key, mask in events:
-                    if key.fileobj == self.server_socket:
-                        self.accept_client()
-                    else:
-                        self.handle_game_logic(key, mask)
-        finally:
-            self.cleanup_all_connections()
-
-    def cleanup_all_connections(self):
-        for client in list(self.clients.keys()):
-            self.disconnect_client(client)
-        self.server_socket.close()
+        while True:
+            self.check_timeout()
+            events = self.selector.select(timeout=1)
+            for key, mask in events:
+                if key.fileobj == self.server_socket:
+                    self.accept_client()
+                else:
+                    self.handle_game_logic(key, mask)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Run a Tic-Tac-Toe server.')
-    parser.add_argument('host', type=str, help='Host to bind the server to.')
-    parser.add_argument('port', type=int, help='Port to bind the server to.')
+    parser = argparse.ArgumentParser(description="Run a Tic-Tac-Toe server.")
+    parser.add_argument('-p', '--port', type=int, required=True, help="Port to bind the server to.")
     args = parser.parse_args()
 
-    server = TicTacToeServer(args.host, args.port)
+    server = TicTacToeServer(args.port)
     server.start_server()
     server.run()
